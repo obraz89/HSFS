5109d196ede5ec0e65068a93f20dc6a145db54d2
Anton Obraz <obraz89@gmail.com>
2011-02-20 20:27:38 +0300

    new version of fortran stabsolver core NOTE: most probably will not be dummy rewritten in ++ but is definitely easier to read

--- Solver_C/include/StabSolver.h
+++ Solver_C/include/StabSolver.h
@@ -1,7 +1,8 @@
 #include "SmProfile.h"
 #include "MF_Field.h"
+#include "StabField.h"
 class StabSolver{
-// TODO?: make SmProfile inside StabSolver if it is not used anywhere else
+	// TODO: get SmPRofile out of Stabsolver
 const MF_Field& fld_ref; // to get field parameters and initialize profiles
 const int i_ind, k_ind;
 SmProfile profile;
@@ -12,4 +13,9 @@ public:
 	void searchGlobal();
 	void smoothProfile();
 	void adaptProfile();
+
+	// form matrix 8x8 of stab eqs at a given ? [y or index]
+	double** form_matrix(double y); 
+	// core function
+	double solve(const StabDataPoint& stab_point);
 };
---
+++ Solver_C/source/Core.cpp
@@ -0,0 +1,14 @@
+// 3d Compressible BL stability equations 
+// parallel flow assumption (*)
+// Originally Developed by Fedrorov A.V., 12.11.1985
+// Do not distribute without Fedorov permission
+
+// the function solves system(*) with the input parameters
+// and returns the determinant of residual
+
+#include "StabField.h"
+#include "StabSolver.h"
+
+double StabSolver::solve(const StabDataPoint& stab_point){
+	return 0;
+}
