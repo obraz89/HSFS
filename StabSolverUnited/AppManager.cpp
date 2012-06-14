ac54545a8b760113c1058eef8e1972e5992d5dcf
Obraz <Obraz89@gmail.com>
2012-06-14 13:11:36 +0300

    add application manager
    read from configfile what components to load

---
+++ StabSolverUnited/AppManager.cpp
@@ -0,0 +1,84 @@
+#include "AppManager.h"
+#include "MeanFlow.h"
+#include "StabSolver.h"
+#include "EigenGs.h"
+#include "common_data.h"
+
+t_AppManager::t_AppManager(wxString task_configfile){
+	wxFileConfig& conf = _get_config_handle(task_configfile);
+	conf.SetRecordDefaults();
+	_add_mf(conf);
+	_add_stab_solver(conf);
+	_add_eigen_gs(conf);
+};
+
+wxFileConfig t_AppManager::_get_config_handle(wxString configfile){
+	return wxFileConfig(
+		_T("SSU"), _T("obraz"),
+		configfile, 
+		wxEmptyString);
+}
+
+void t_AppManager::_add_mf(wxFileConfig& conf){
+	wxString mf_type;
+	conf.Read(_T("MF_Type"), &mf_type, MF_HSFLOW3D_NAME);
+	if (mf_type==MF_HSFLOW3D_NAME){
+		_pMF = new t_MFHSFLOW3D();
+	}else{
+		if (mf_type==MF_HSFLOW2D_NAME){
+			_pMF = new t_MFHSFLOW2D();
+		}else{
+			throw e_UnsuppComp();
+		};
+	};
+	_add_component(dynamic_cast<t_Component*>(_pMF));
+};
+
+void t_AppManager::_add_stab_solver(wxFileConfig& conf){
+	// for now it is the only stab solver:
+	wxString ss_type;
+	conf.Read(_T("StabSolver_Type"), &ss_type, STABSOLVER3D_NAME);
+	_pStabSolver = new t_StabSolver(*_pMF);
+	_add_component(_pStabSolver);
+};
+
+void t_AppManager::_add_eigen_gs(wxFileConfig& conf){
+	// for now it is the only stab solver:
+	wxString gs_type;
+	conf.Read(_T("EigenGS_Type"), &gs_type, EIGEN3D_NAME);
+	_pEigenGS = new t_EigenGS(*_pMF);
+	_add_component(_pEigenGS);
+};
+
+void t_AppManager::_add_component(t_Component* pComp){
+	const wxString& name = pComp->name();
+	wxASSERT_MSG( _mapComponents.find(name)==_mapComponents.end(),
+		_("Component '")+name+_("' already added'")+_("'.")
+		);
+	_mapComponents.insert( std::make_pair(name, pComp) );
+};
+
+void t_AppManager::load_settings(wxString configfile){
+	std::map<wxString, t_Component*>::iterator it = _mapComponents.begin();
+	while (it!=_mapComponents.end()){
+		try{
+			std::cout<<"Initializing:"<<it->second->name().c_str()<<std::endl;
+			it->second->initialize(configfile);
+			it++;
+		}catch(const t_EComponent& e){
+			#ifdef _DEBUG
+				wxLogError(e.what_detailed());
+			#else
+				wxLogError(e.what());
+			#endif
+		};
+	};
+};
+
+t_AppManager::~t_AppManager(){
+	std::map<wxString, t_Component*>::iterator it = _mapComponents.begin();
+	while (it!=_mapComponents.end()){
+		delete it->second;
+		it++;
+	};
+};
--- StabSolverUnited/include/AppManager.h
+++ StabSolverUnited/include/AppManager.h
@@ -1,1 +1,37 @@
+#ifndef __APP_MANAGER
+#define __APP_MANAGER
+
 //#include "wx/wx.h"
+#include "stdafx.h"
+#include "wx/fileconf.h"
+#include "component.h"
+class t_MeanFlow;
+class t_StabSolver;
+class t_EigenGS;
+
+class t_AppManager{
+	std::map<wxString, t_Component*> _mapComponents;
+	t_MeanFlow*  _pMF;
+	t_StabSolver* _pStabSolver;
+	t_EigenGS* _pEigenGS;
+	wxFileConfig _get_config_handle(wxString configfile);
+	void _add_mf(wxFileConfig&);
+	void _add_stab_solver(wxFileConfig&);
+	void _add_eigen_gs(wxFileConfig&);
+	void _add_component(t_Component*);
+public:
+	// add components from task config file
+	t_AppManager(wxString configfile);
+	t_MeanFlow& get_mf(){return *_pMF;};
+	t_StabSolver& get_stab_solver(){return *_pStabSolver;};
+	t_EigenGS& get_eigen_gs(){return *_pEigenGS;};
+	// load settings for components from param config file
+	void load_settings(wxString configfile);
+	// ?
+	// void save_settings(); 
+	~t_AppManager();
+	// exceptions
+	class e_UnsuppComp{};
+};
+
+#endif // __APP_MANAGER
