0511b4b79f99341ff586d38f90a2c16c130a313c
Obraz <Obraz89@gmail.com>
2012-06-14 13:18:16 +0300

    update common data - names and domains

--- shared/include/common_data.h
+++ shared/include/common_data.h
@@ -1,2 +1,10 @@
-const wxString MF_CONF_DOMAIN = _T("MeanFlow");
-//const wxString t_MFParams::ConfigDomain = _T("MeanFlow");
+#include "wx/string.h"
+
+extern const wxString MF_HSFLOW3D_NAME;
+extern const wxString MF_HSFLOW2D_NAME;
+extern const wxString EIGEN3D_NAME;	
+extern const wxString STABSOLVER3D_NAME;
+
+extern const wxString MF_CONF_DOMAIN;
+extern const wxString EIGEN_CONF_DOMAIN;
+extern const wxString STABSOLVER_CONF_DOMAIN;
---
+++ shared/source/common_data.cpp
@@ -0,0 +1,11 @@
+#include "common_data.h"
+// component names
+const wxString MF_HSFLOW3D_NAME = _T("MF_HSFLOW3D");
+const wxString MF_HSFLOW2D_NAME = _T("MF_HSFLOW2D");
+const wxString STABSOLVER3D_NAME= _T("StabSolver");
+const wxString EIGEN3D_NAME = _T("EigenGS");
+
+// config domains
+const wxString MF_CONF_DOMAIN = _T("MeanFlow");
+const wxString STABSOLVER_CONF_DOMAIN = _T("StabSolver");
+const wxString EIGEN_CONF_DOMAIN= _T("EigenGS");
