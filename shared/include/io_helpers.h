be674f0f1db687d7e9d63a0b341ed68f9da13297
Obraz <Obraz89@gmail.com>
2012-08-23 15:00:58 +0400

    update io helpers

--- shared/include/io_helpers.h
+++ shared/include/io_helpers.h
@@ -16,6 +16,15 @@ namespace std_manip{
 		os.flags(old_flags);
 		return os;
 	};
+
+	template<typename T> inline std::wostream& _format_sci(std::wostream& os, T val){
+		os.width(FIELD_WIDTH_DEFAULT);
+		os.precision(PRECISION_DEFAULT);
+		int old_flags = os.flags(std::ios::left|std::ios::scientific);
+		os<<val;
+		os.flags(old_flags);
+		return os;
+	};
 	template<typename T>class t_Omanip{
 		T _val;
 		std::wostream& (*_formatter)(std::wostream &os, T val);
@@ -26,6 +35,10 @@ namespace std_manip{
 	};
 	// TODO:think how to introduce options
 	// and make this all useful)))
-	template<typename T>inline t_Omanip<T> std_format_fixed(T val){return t_Omanip<T>(val,_format_fixed<T>);}; 
+	template<typename T>inline t_Omanip<T> 
+		std_format_fixed(T val){return t_Omanip<T>(val,_format_fixed<T>);}; 
+
+	template<typename T>inline t_Omanip<T> 
+		std_format_sci(T val){return t_Omanip<T>(val,_format_sci<T>);}; 
 };
 #endif // __IO_HELPERS
