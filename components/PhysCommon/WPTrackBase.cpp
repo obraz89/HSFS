96f589d7f21bb9a2a379fddb06e1b847937fcd4e
Obraz <Obraz89@gmail.com>
2014-03-11 11:28:59 +0300

    [add]: Add stab database sketch

--- components/PhysCommon/WPTrackBase.cpp
+++ components/PhysCommon/WPTrackBase.cpp
@@ -4,6 +4,108 @@
 
 using namespace stab;
 
+t_WPLineRec::t_WPLineRec(const mf::t_Rec& rMF, const t_WCharsGlob& rWC):
+mean_flow(rMF), wave_chars(rWC){};
+
+
+/*std::wostream& operator<<(std::wostream& str, t_WavePackLine::t_WPLineRec rec){
+
+	str<<_T("hi! Implement me, please\n");
+	return str;
+
+};*/
+
 t_WPTrackBase::t_WPTrackBase(){};
 
 t_WPTrackBase::~t_WPTrackBase(){};
+
+int t_StabDBase::get_npoints() const{return _pave_pts.size();};
+
+const t_PavePoint& t_StabDBase::get_pave_pt(int ind) const{
+
+	return _pave_pts[ind];
+
+};
+
+
+void t_StabDBase::init_pave_pts(const std::vector<mf::t_GeomPoint>& pnts){
+
+	int nsize = pnts.size();
+	_pave_pts.resize(nsize);
+
+	for (int i=0; i<nsize; i++) _pave_pts[i].xyz = pnts[i];
+
+}
+
+t_PavePoint& t_StabDBase::_get_nrst_pnt(const mf::t_GeomPoint& xyz){
+
+	t_PavePoint* p_ret_point;
+	int nn = _pave_pts.size();
+	double min_dst = 1.0e+18;
+	t_Vec3Dbl dr;
+	double cur_dst;
+
+	for (int i=0; i<nn; i++){
+
+		matrix::base::minus<double, double>(_pave_pts[i].xyz, xyz, dr);
+		cur_dst = dr.norm();
+
+		if (cur_dst<min_dst){
+
+			min_dst = cur_dst; 
+			p_ret_point = &_pave_pts[i];
+		}
+
+	}
+
+	return *p_ret_point;
+
+
+}
+
+// IMPORTANT TODO: do not update rec if it is not very close to pave point !!!
+void t_StabDBase::update(const t_WPTrackBase& wpline){
+
+	for (int i=0; i<wpline.get_size(); i++){
+
+		const t_WPLineRec& wprec = wpline.get_rec(i);
+		t_PavePoint& pave_point = _get_nrst_pnt(wprec.mean_flow.get_xyz());
+
+		if (wprec.n_factor>pave_point.max_N) pave_point.max_N = wprec.n_factor;
+		// ...
+	}
+}
+
+// IMPORTANT TODO: transform t_WaveChars and others... now need 
+// only xyz and N for testing
+void t_StabDBase::to_cone_ref_frame(double half_angle){
+
+	std::vector<t_PavePoint>::iterator it;
+
+	//t_Vec3Dbl cur_vec;
+	//t_Vec3Cmplx cur_k_vec;
+
+	for (it=_pave_pts.begin(); it<_pave_pts.end(); it++){
+
+		smat::vec_cart_to_cone(it->xyz, half_angle);
+
+	}
+
+}
+
+void t_StabDBase::export(const std::wstring& fname) const{
+
+	std::wofstream fstr(&fname[0]);
+	fstr<<_T("x[m]\ty[m]\tz[m]\tN[]\n");
+
+	for (int i=0; i<_pave_pts.size(); i++){
+		const mf::t_GeomPoint& xyz = _pave_pts[i].xyz;
+		fstr<<_T("\t")<<xyz[0]
+			<<_T("\t")<<xyz[1]
+			<<_T("\t")<<xyz[2]
+			<<_T("\t")<<_pave_pts[i].max_N<<_T("\n");
+	}
+
+	fstr.close();
+
+}
--- components/PhysCommon/WPTrackBase.h
+++ components/PhysCommon/WPTrackBase.h
@@ -9,7 +9,34 @@
 
 #include "dll_impexp-phys_common.h"
 
+
+
+
 namespace stab{
+
+/************************************************************************/
+/* Record of a Wave Pack Line in a given point xyz
+// store mean flow data (including xyz);
+// wave chars in global cartesian ref frame;
+// calculated n factor
+/************************************************************************/
+
+	struct IMPEXP_PHYSCOMMON t_WPLineRec{
+
+		mf::t_Rec mean_flow;
+		t_WCharsGlob wave_chars;
+		double n_factor;
+
+		t_WPLineRec(const mf::t_Rec& rMF, const t_WCharsGlob& rWC);
+
+		//friend std::wostream& operator<<(std::wostream& str, t_WPLineRec rec);
+	};
+
+/************************************************************************/
+/* Interface to Wave Packet Trajectories
+// Base method is to retrace given 
+// wpline type from a point
+/************************************************************************/
 
 	class IMPEXP_PHYSCOMMON t_WPTrackBase: public hsstab::TPlugPhysPart{
 	public:
@@ -19,9 +46,61 @@ namespace stab{
 		virtual void retrace(mf::t_GeomPoint start_from, t_WCharsLoc init_wave, 
 			stab::t_LSBase& loc_solver)=0;
 
+		virtual void calc_n_factor()=0;
+
+// some useful transforms into various reference frames
+		// cylindrical - x, phi, r
+		virtual void to_cyl_ref_frame()=0;
+
+		// conical - l, phi, h
+		// for points on surface h=0
+		// l is along generator
+		virtual void to_cone_ref_frame(double half_angle)=0;
+
+		virtual int get_size() const =0;
+
+		virtual const stab::t_WPLineRec& get_rec(int ind) const=0;
+
 		virtual void print_to_file(const std::wstring& fname, int write_mode) const=0;
 
 		virtual ~t_WPTrackBase();
+	};
+
+/************************************************************************/
+/* Sketch of a "Stability Database" over 3D configuration
+// Just a container for Pave points - Records including:
+// the result of Global search at given Pave Point 
+// max N Factor of all WPLines that cover Pave Point
+// ...
+// TODO: IO for a real cgns or separate db...
+/************************************************************************/
+
+	struct IMPEXP_PHYSCOMMON t_PavePoint{
+
+		mf::t_GeomPoint xyz;
+		std::vector<t_WCharsGlobDim> wchars;
+		double max_N;
+
+	};
+
+	class IMPEXP_PHYSCOMMON t_StabDBase{
+
+		std::vector<t_PavePoint> _pave_pts;
+
+		t_PavePoint& _get_nrst_pnt(const mf::t_GeomPoint& xyz);
+
+	public:
+
+		int get_npoints() const;
+		const t_PavePoint& get_pave_pt(int ind) const;
+
+		// tmp, decide how to initialize in general case
+		void init_pave_pts(const std::vector<mf::t_GeomPoint>& pnts);
+		void update(const t_WPTrackBase& wpline);
+
+		void to_cone_ref_frame(double half_angle);
+		void export(const std::wstring& fname) const;
+
 	};
 
 };
