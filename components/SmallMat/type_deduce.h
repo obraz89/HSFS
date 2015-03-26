bb2fd2ff934ea6c476d26b4cd88146b46b6c6e9c
Obraz <Obraz89@gmail.com>
2015-03-26 17:28:57 +0300

    [upd]: makefile for smallmat, fixing linux icpc complaying about << in templates

--- components/SmallMat/type_deduce.h
+++ components/SmallMat/type_deduce.h
@@ -7,19 +7,19 @@ namespace matrix{
 
 	template<> class TypeDeduce<int, int>{public:typedef int type;};
 	template<> class TypeDeduce<int, double>{public:typedef double type;};
-	template<> class TypeDeduce<int, std::complex<double>>{
+	template<> class TypeDeduce<int, std::complex<double> >{
 		public:typedef std::complex<double> type;};
 
 	template<> class TypeDeduce<double, int>{public:typedef double type;};
 	template<> class TypeDeduce<double, double>{public:typedef double type;};
-	template<> class TypeDeduce<double, std::complex<double>>{
+	template<> class TypeDeduce<double, std::complex<double> >{
 		public:typedef std::complex<double> type;};
 
 	template<> class TypeDeduce<std::complex<double>, int>{
 		public:typedef std::complex<double> type;};
 	template<> class TypeDeduce<std::complex<double>, double>{
 		public:typedef std::complex<double> type;};
-	template<> class TypeDeduce<std::complex<double>, std::complex<double>>{
+	template<> class TypeDeduce<std::complex<double>, std::complex<double> >{
 		public:typedef std::complex<double> type;};
 
 };
