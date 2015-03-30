763211564f921b6cb5350f08a8457f43919acbee
Obraz <Obraz89@gmail.com>
2015-03-30 20:32:39 +0300

    [add/upd]: add makefiles for majority of plugins
    not all tested under linux

--- build_linux/make_all.sh
+++ build_linux/make_all.sh
@@ -5,6 +5,16 @@
 
 PROJ='
 ../shared
+../components/SmallMat
+../components/PhysCommon
+../components/Profile
+../components/MF.CGNS-shared
+../components/MF.CGNS2D
+../components/MF.CGNS3D
+../components/PF.GlobSearch_Lapack
+../components/PF.LocSearch
+../components/WPTrack
+../HSFLowStab-main
 '
 
 for p in $PROJ
