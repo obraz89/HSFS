187d4fed321b8fa346d10f1c8b9ea12c894682c0
obraz <obraz89@gmail.com>
2017-04-10 10:43:13 +0300

    [+]: scripts to run program under Win

---
+++ start.cmd
@@ -0,0 +1,21 @@
+
+:: @if not "%WindowsSdkDir%" == "" (
+::	set "PATH=%WindowsSdkDir%bin;%PATH%"
+::	set "INCLUDE=%WindowsSdkDir%include;%INCLUDE%"
+::	set "LIB=%WindowsSdkDir%lib;%LIB%"
+::)
+@set HSFLOWStabBin=%CD%/bin
+@set HSFSRoot=E:/science/devel/HSFS
+
+@set WX_DIR=%HSFSRoot%\lib_vc14\wx-vc14-rlz-win64
+@set WX_ARCH=vc140_x64_dll
+
+@set PATH=%PATH%;%INTEL_COMPILERS%/bin/intel64/;%WX_DIR%/lib/%WX_ARCH%;E:\lib\MSMPI_v8\bin
+
+::@if %HSSTAB_CASE_DIR%.==. set HSSTAB_CASE_DIR=%CD% 
+
+::@start %HSFlowStabRoot%/StabSolverUnited.sln
+::call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat amd64"
+
+::start "M= 4 wedge aoa=0 Re1=7.381x10^6 " /LOW cmd /K "%HSFLOWStabBin%\HSFlowStab-main.exe -l log.txt ."
+start "M= 3.5 plate aoa=0" /LOW cmd /K "mpiexec -n 1 %HSFLOWStabBin%\HSFlowStab-main.exe -l log.txt ."
---
+++ start_debug.cmd
@@ -0,0 +1,32 @@
+
+:: copy this file to your case dir and launch !!!
+
+:: Change %HSFSRoot% to your HSFS project root folder !!!
+
+@set HSFSRoot=E:/science/devel/HSFS
+
+@set HSFSBin=%HSFSRoot%/bin_x64_Debug
+@set HSFLOWStabBin=%HSFSBin%
+
+@if %HSSTAB_CASE_DIR%.==. set HSSTAB_CASE_DIR=%CD% 
+
+:: set paths to required libs
+
+@set WX_DIR=%HSFSRoot%\lib_vc14\wx-vc14-rlz-win64
+@set WX_ARCH=vc140_x64_dll
+
+::@set MPICH2_DIR=%HSFSRoot%/lib_vc14/msmpi-win64
+::@set MPICH2_DIR=E:\lib\MSMPI_v8
+@set MPICH2_DIR=E:\lib\MSMPI_v8
+
+@set HDF5_DIR=%HSFSRoot%/lib_vc14/hdf5_1.1
+
+@set CGNS_DIR=E:\science\devel\HSFS\lib_vc14\cgns-vc14-rlz-win64
+
+@set MKL_DIR=%HSFSRoot%/lib/mkl-xe-2015
+
+@set PATH=%PATH%;%INTEL_COMPILERS%/bin/intel64;%WX_DIR%/lib/%WX_ARCH%/
+
+@start %HSFSRoot%/StabSolverUnited.sln
+
+===============================
