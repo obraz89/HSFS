ae00ee0940d1c492eb7c4093fd37c86a10091a13
obraz <obraz89@gmail.com>
2017-12-19 17:06:51 +0300

    [+]: fix retrace MPI - wb_fixed is "simplified", wb_rad should treat all modes independent

--- HSFLowStab-main/tasks_wp.cpp
+++ HSFLowStab-main/tasks_wp.cpp
@@ -18,7 +18,8 @@ using namespace hsstab;
 #define NMAX_FNAME_LEN 100
 #define FILENAME_H5    "output/stab.h5"
 
-void retrace_single_WP(int wp_id, stab::t_WPRetraceMode a_mode_retrace, t_WPLine2H5Arr& a_arr);
+void retrace_single_WP(const int wp_id, const int ppid, const bool a_do_simple_retrace, 
+	const stab::t_WPRetraceMode a_mode_retrace, t_WPLine2H5Arr& a_arr);
 
 void write_wpdata(hid_t file, hid_t group, char* dsname, const t_WPLine2H5Arr& arr);
 void read_wpdata(hid_t file, char* ds_abs_name, t_WPLine2H5Arr& arr);
@@ -37,7 +38,11 @@ void task::retrace_MPI(stab::t_WPRetraceMode a_mode_retrace) {
 	// distribute wps through workers
 	const task::TTaskParams& gtp = g_taskParams;
 
-	const int NWPGlob = gtp.N_b * gtp.N_w;
+	const bool do_simple_retrace = (a_mode_retrace == stab::t_WPRetraceMode::WB_FIXED);
+
+	const int npp = g_pStabDB->get_npoints();
+
+	const int NWPGlob = do_simple_retrace ? gtp.N_b * gtp.N_w : gtp.N_b*gtp.N_w*npp;
 
 	MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
@@ -66,6 +71,21 @@ void task::retrace_MPI(stab::t_WPRetraceMode a_mode_retrace) {
 
 	}
 
+	int* wpid_to_ppid_map = new int[NWPGlob];
+	
+	if (!do_simple_retrace) {
+
+		for (int k = 0; k<npp; k++)
+			for (int j = 0; j<gtp.N_w; j++)
+				for (int i = 0; i < gtp.N_b; i++) {
+					int wpid = k*gtp.N_w*gtp.N_b + j*gtp.N_w + i;
+					wpid_to_ppid_map[wpid] = k;
+		}
+
+	}
+	else { for (int i = 0; i < NWPGlob; i++) wpid_to_ppid_map[i] = -1; }
+
+
 	// retrace & pack
 
 	t_WPLine2H5Arr* arr_pack = new t_WPLine2H5Arr[NWPLoc];
@@ -74,7 +94,7 @@ void task::retrace_MPI(stab::t_WPRetraceMode a_mode_retrace) {
 
 		wxLogMessage(_T("rank=%d, starting retrace wpid=%d, wpid_s=%d, wpid_e=%d"), mpi_rank, wpid, wpid_s, wpid_e);
 
-		retrace_single_WP(wpid, a_mode_retrace, arr_pack[wpid-wpid_s]);
+		retrace_single_WP(wpid, wpid_to_ppid_map[wpid], do_simple_retrace, a_mode_retrace, arr_pack[wpid-wpid_s]);
 
 	}
 
@@ -159,7 +179,112 @@ void task::retrace_MPI(stab::t_WPRetraceMode a_mode_retrace) {
 
 }
 
-void retrace_single_WP(int wpid, stab::t_WPRetraceMode a_mode_retrace, t_WPLine2H5Arr& a_arr) {
+bool search_init_wave(const int wpid, const int a_ppoint_id, const bool a_do_simple_retrace,
+	const stab::t_WPRetraceMode a_mode_retrace, t_WCharsLoc& a_wchars, mf::t_GeomPoint& a_start_xyz) {
+
+	bool gs_success = false;
+
+	// npp - number of pave point
+	const int npp = g_pStabDB->get_npoints();
+
+	const task::TTaskParams& gtp = g_taskParams;
+
+	const int Nw = gtp.N_w;
+	const int Nb = gtp.N_b;
+
+	int i, j;
+
+	if (a_do_simple_retrace) {
+
+		i = wpid % Nw;
+		j = wpid / Nw;
+
+	}
+	else {
+
+		j = wpid / (npp*Nw);
+		i = (wpid - j*npp*Nw) / npp;
+
+	};
+
+	double db_dim = (gtp.N_b > 1) ? (gtp.b_dim_max - gtp.b_dim_min) / double(gtp.N_b - 1) : 0.0;
+	double dw_dim = (gtp.N_w > 1) ? (gtp.w_dim_max - gtp.w_dim_min) / double(gtp.N_w - 1) : 0.0;
+
+	double b_ldim = gtp.b_dim_min + j*db_dim;
+	double w_ldim = gtp.w_dim_min + i*dw_dim;
+
+	// if doing simplified retrace search for instab at all pave points until instab is found
+	// if doing exact retrace, do gs at a given point
+
+	const int npp_s = a_do_simple_retrace ? npp - 1 : a_ppoint_id;
+	const int npp_e = a_do_simple_retrace ? 0 : a_ppoint_id;
+
+	for (int npp = npp_s; npp >= npp_e; npp--) {
+
+		try {
+
+			const mf::t_GeomPoint& test_xyz = g_pStabDB->get_pave_pt(npp).xyz;
+
+			g_pGSSolverSpat->setContext(test_xyz);
+
+			g_pStabSolver->setContext(test_xyz);
+
+			const t_StabScales& sc = g_pStabSolver->get_stab_scales();
+
+			t_WCharsLocDim wch_ldim;
+			wch_ldim.set_treat(stab::t_TaskTreat::SPAT);
+			wch_ldim.set_scales(sc);
+
+			wch_ldim.a = 0.0;
+			wch_ldim.b = b_ldim;
+			wch_ldim.w = w_ldim;
+
+			t_WCharsLoc wchars = wch_ldim.to_nondim(sc);
+
+			gs_success = false;
+			stab::t_LSCond cond(stab::t_LSCond::B_FIXED | stab::t_LSCond::W_FIXED);
+			wxLogMessage(_T("start gs for WPLine = [i_w=%d, j_b=%d]\n\t pave_point=%d"), i, j, a_ppoint_id);
+			{
+				std::vector<t_WCharsLoc> init_waves_raw;
+				std::vector<t_WCharsLoc> init_waves_filtered;
+
+				init_waves_raw = g_pGSSolverSpat->getInstabModes(wchars);
+
+				init_waves_filtered = g_pStabSolver->filter_gs_waves_spat(init_waves_raw, cond);
+
+				if (init_waves_filtered.size()>0) {
+
+					gs_success = true;
+					a_wchars = t_WCharsLoc::find_max_instab_spat(init_waves_filtered);
+					a_start_xyz = test_xyz;
+					return true;
+
+				}
+
+			}
+
+
+
+		}
+		catch (const t_GenException& ex) {
+			wxLogMessage(ex.what());
+			return false;
+		}
+		catch (...) {
+
+			wxLogMessage(_T("RetraceMPI Error, WPLine = [i_w=%d, j_b=%d]\n\t pave_point=%d"), i, j, npp);
+			return false;
+		}
+
+	}	// ~pave points loop
+
+	return false;
+
+}
+
+
+void retrace_single_WP(const int wpid, const int ppid, const bool a_do_simple_retrace,
+	stab::t_WPRetraceMode a_mode_retrace, t_WPLine2H5Arr& a_arr) {
 
 	char szFname[NMAX_FNAME_LEN];
 	char fout_maxnfactor_str[NMAX_FNAME_LEN];
@@ -181,78 +306,23 @@ void retrace_single_WP(int wpid, stab::t_WPRetraceMode a_mode_retrace, t_WPLine2
 
 	const task::TTaskParams& gtp = g_taskParams;
 
-	// npp - number of pave point
-	// move from back to leading edge (assuming x increasing in pave points)
-	const int npp_s = g_pStabDB->get_npoints() - 1;
-	const int npp_e = 0;
-
-	const int Nw = gtp.N_w;
-	const int Nb = gtp.N_b;
-
-	int i, j;
-
-	i = wpid % Nw;
-	j = wpid / Nw;
-
 	{
 
-		 double db_dim = (gtp.N_b > 1) ? (gtp.b_dim_max - gtp.b_dim_min) / double(gtp.N_b - 1) : 0.0;
-		 double dw_dim = (gtp.N_w > 1) ? (gtp.w_dim_max - gtp.w_dim_min) / double(gtp.N_w - 1) : 0.0;
-
-		 double b_ldim = gtp.b_dim_min + j*db_dim;
- 		 double w_ldim = gtp.w_dim_min + i*dw_dim;
-
-		 for (int npp = npp_s; npp >= npp_e; npp--) {
-
+	
+		t_WCharsLoc wchars;
+		mf::t_GeomPoint start_xyz;
 			try {
 
-				const mf::t_GeomPoint& test_xyz = g_pStabDB->get_pave_pt(npp).xyz;
-
-				g_pGSSolverSpat->setContext(test_xyz);
-
-				g_pStabSolver->setContext(test_xyz);
-
-				const t_StabScales& sc = g_pStabSolver->get_stab_scales();
-
-				t_WCharsLocDim wch_ldim;
-				wch_ldim.set_treat(stab::t_TaskTreat::SPAT);
-				wch_ldim.set_scales(sc);
-
-				wch_ldim.a = 0.0;
-				wch_ldim.b = b_ldim;
-				wch_ldim.w = w_ldim;
-
-				t_WCharsLoc wchars = wch_ldim.to_nondim(sc);
-
-				// do global search of instab
-				bool gs_success = false;
-				stab::t_LSCond cond(stab::t_LSCond::B_FIXED | stab::t_LSCond::W_FIXED);
-				wxLogMessage(_T("start gs for WPLine = [i_w=%d, j_b=%d]\n\t pave_point=%d"), i, j, npp);
-				{
-					std::vector<t_WCharsLoc> init_waves_raw;
-					std::vector<t_WCharsLoc> init_waves_filtered;
-
-					init_waves_raw = g_pGSSolverSpat->getInstabModes(wchars);
-
-					init_waves_filtered = g_pStabSolver->filter_gs_waves_spat(init_waves_raw, cond);
-
-					if (init_waves_filtered.size()>0){
-
-						gs_success = true;
-						wchars = t_WCharsLoc::find_max_instab_spat(init_waves_filtered);
-
-					}
-
-				}
+				bool gs_ok = search_init_wave(wpid, ppid, a_do_simple_retrace, a_mode_retrace, wchars, start_xyz);
 
 				// Init wave found, retrace WP
-				if (gs_success) {
+				if (gs_ok) {
 
 					wxLogMessage(_T("\tstart retrace for WPLine"));
 
 					std::wcout << _T("Init for wpline:") << wchars;
 
-					wp_line->retrace(test_xyz, wchars, *g_pStabSolver, *g_pGSSolverSpat, a_mode_retrace);
+					wp_line->retrace(start_xyz, wchars, *g_pStabSolver, *g_pGSSolverSpat, a_mode_retrace);
 
 					wp_line->print_to_file(fout_wplines_str, std::ios::app);
 
@@ -262,7 +332,6 @@ void retrace_single_WP(int wpid, stab::t_WPRetraceMode a_mode_retrace, t_WPLine2
 
 					// no need to retrace same WP from different points
 					// correct only for 2D configurations
-					break;	
 
 				}
 				else {
@@ -275,10 +344,8 @@ void retrace_single_WP(int wpid, stab::t_WPRetraceMode a_mode_retrace, t_WPLine2
 			}
 			catch (...) {
 
-				wxLogMessage(_T("RetraceMPI Error, WPLine = [i_w=%d, j_b=%d]\n\t pave_point=%d"), i, j, npp);
+				wxLogMessage(_T("RetraceMPI Error, WPLine=%d]\n\t pave_point=%d"), wpid, ppid);
 			}
-
-		}	// ~pave points loop
 
 		
 	}	// ~WP Lines loop
