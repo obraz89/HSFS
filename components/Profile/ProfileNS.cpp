d41ab969e97ad8240e797f666ae2f1ec54054315
obraz <obraz89@gmail.com>
2021-01-15 18:46:21 +0300

    [+]: profile mf loc: rotate mf derivs to local rf

--- components/Profile/ProfileNS.cpp
+++ components/Profile/ProfileNS.cpp
@@ -29,9 +29,13 @@ void t_ProfMFLoc::_initialize_extract(const t_GeomPoint& xyz, const mf::t_ProfDa
 	_xyz = r_xyz_base;
 
 	t_SqMat3Dbl jac = _rDomain.calc_jac_to_loc_rf(r_xyz_base);
-	t_SqMat3Dbl inv_jac = jac.inverse();
+	t_SqMat3Dbl jac_inv = jac.inverse();
 
 	const mf::t_FldParams& Params = _rDomain.get_mf_params();
+
+	t_SqMat3Dbl jac_t = jac; jac_t.transpose();
+	t_SqMat3Dbl jac_inv_t = jac_inv; jac_inv_t.transpose();
+	t_SqMat3Dbl du_dx, du1_dk, mat_tmp;
 
 	for (int j = 0; j<_nnodes; j++) {
 
@@ -48,12 +52,10 @@ void t_ProfMFLoc::_initialize_extract(const t_GeomPoint& xyz, const mf::t_ProfDa
 			_y[j] = _y[j - 1] + dr_xyz.norm();
 		}
 
-		//r_xyz.set(mf_rec);
-		//matrix::base::minus<double, double>(r_xyz, r_xyz_base, dr_xyz);
-		//matrix::base::mat_mul<double, double>(inv_jac, dr_xyz, r_ked);
+		// step 1 - rotate mf records
 
 		u_xyz.set(mf_rec.u, mf_rec.v, mf_rec.w);
-		matrix::base::mat_mul<double, double>(inv_jac, u_xyz, u_ked);
+		matrix::base::mat_mul<double, double>(jac_inv, u_xyz, u_ked);
 
 		// TODO: check that v is small
 		_u[j] = u_ked[0];
@@ -66,6 +68,36 @@ void t_ProfMFLoc::_initialize_extract(const t_GeomPoint& xyz, const mf::t_ProfDa
 		double gMaMa = Params.Gamma*pow(Params.Mach, 2);
 		_r[j] = mf_rec.p / mf_rec.t*gMaMa;
 		_mu[j] = _rDomain.calc_viscosity(_t[j]);
+
+		// step 2 - rotate mf rec derivatives (extracted from mf domain)
+		// from global to local reference frame
+
+		// velocity component derivatives: 
+		// du1_dk - matrix of new velo derivs
+		// du1_dk = Jac_inv * du_dx * Jac
+		// it is convenient to transpose:
+		// du1_dk_T = Jac_T * (du_dx_T) * Jac_inv_T
+		// then columns of du1_dk_T are result vectors
+		du_dx.set_col(0, prof_derivs[j].ug);
+		du_dx.set_col(1, prof_derivs[j].vg);
+		du_dx.set_col(2, prof_derivs[j].wg);
+
+		matrix::base::mat_mul<double, double>(jac_t, du_dx, mat_tmp);
+		matrix::base::mat_mul<double, double>(mat_tmp, jac_inv_t, du1_dk);
+
+		// the result is du1_dk:
+		// (du1_dksi, du2_dksi, du3_dksi)
+		// (du1_deta, du2_deta, du3_deta)
+		// (du1_dzet, du2_dzet, du3_dzet)
+		// (u1, u2, u3) - components of velocity vector in local reference frame
+		du1_dk.col_to_vec(0, _prof_derivs[j].ug);
+		du1_dk.col_to_vec(1, _prof_derivs[j].vg);
+		du1_dk.col_to_vec(2, _prof_derivs[j].wg);
+
+		// rotate scalars - pressure and temperature
+		matrix::base::mat_mul<double, double>(jac_t, prof_derivs[j].pg, _prof_derivs[j].pg);
+		matrix::base::mat_mul<double, double>(jac_t, prof_derivs[j].tg, _prof_derivs[j].tg);
+
 	}
 
 	_calc_derivs();
@@ -398,11 +430,12 @@ void t_ProfMFLoc::dump(const std::string& fname) const {
 
 	const double rue_1 = 1.0 / (rec_out.r*rec_out.u);
 
-	fstr << _T("y\tu\tu'\tu''\tt\tt'\tt''\tr\tmu\tmu'\tmu''\tw\tw'\tw''\tv\tMach\tdelta**\n");
+	fstr << _T("y\tu\tu'\tu''\tt\tt'\tt''\tr\tmu\tmu'\tmu''\tw\tw'\tw''\tv\tMach\tdelta**\tdu_dy_mf\tdt_dy_mf\n");
 
 	double mach, dd;
 
 	mf::t_Rec mf_rec;
+	mf::t_RecGrad mf_rec_grad;
 
 	std::vector<double> dd_v(get_nnodes());
 	std::vector<double> mthick_v(get_nnodes());
@@ -423,6 +456,8 @@ void t_ProfMFLoc::dump(const std::string& fname) const {
 
 		mf_rec = rec.make_mf_rec();
 
+		mf_rec_grad = _prof_derivs[i];
+
 		mach = _rDomain.calc_mach(mf_rec);
 
 		fstr << rec.y << _T("\t") <<
@@ -430,7 +465,10 @@ void t_ProfMFLoc::dump(const std::string& fname) const {
 			rec.t << _T("\t") << rec.t1 << _T("\t") << rec.t2 << _T("\t") << rec.r << _T("\t") <<
 			rec.mu << _T("\t") << rec.mu1 << _T("\t") << rec.mu2 << _T("\t") <<
 			rec.w << _T("\t") << rec.w1 << _T("\t") << rec.w2 << _T("\t") << rec.v << _T("\t") <<
-			mach << _T("\t") << mthick_v[i] << ("\n");
+			mach << _T("\t") << mthick_v[i] << _T("\t")<<
+			// debug, to compare calculated du_dy and dT_dy with values extracted from mf domain
+			mf_rec_grad.ug[1]<<_T("\t")<<mf_rec_grad.tg[1]<<
+			_T("\n");
 
 
 	}
